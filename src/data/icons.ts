b5279b987b1b15614a06314e936975d7369fa65d
geofannyher <geofannyher@gmail.com>
2024-11-15 13:13:29 +0700

    check

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -5,7 +5,7 @@ export { default as VideocamIcon } from "@mui/icons-material/Videocam";
 export { default as ImportContactsIcon } from "@mui/icons-material/ImportContacts";
 export { default as CakeIcon } from "@mui/icons-material/Cake";
 export { default as Face6Icon } from "@mui/icons-material/Face6";
-export { default as KeyboardArrowRightIcon } from "@mui/icons-material/KeyboardArrowRight";
 export { default as GraphicEqIcon } from "@mui/icons-material/GraphicEq";
+export { default as KeyboardArrowRightIcon } from "@mui/icons-material/KeyboardArrowRight";
 export { default as KeyboardArrowDownIcon } from "@mui/icons-material/KeyboardArrowDown";
 export { default as PaidIcon } from "@mui/icons-material/Paid";
