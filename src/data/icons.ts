a45e865aa009623e9b9b83b32f8d7ad13d627e2b
geofannyher <geofannyher@gmail.com>
2024-11-15 13:11:48 +0700

    check

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -6,6 +6,6 @@ export { default as ImportContactsIcon } from "@mui/icons-material/ImportContact
 export { default as CakeIcon } from "@mui/icons-material/Cake";
 export { default as Face6Icon } from "@mui/icons-material/Face6";
 export { default as KeyboardArrowRightIcon } from "@mui/icons-material/KeyboardArrowRight";
-export { default as KeyboardArrowDownIcon } from "@mui/icons-material/KeyboardArrowDown";
 export { default as GraphicEqIcon } from "@mui/icons-material/GraphicEq";
+export { default as KeyboardArrowDownIcon } from "@mui/icons-material/KeyboardArrowDown";
 export { default as PaidIcon } from "@mui/icons-material/Paid";
